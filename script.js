d325f9e50b6f4da6c1e83a81a678530b1fe18223
Abhismart786 <abhiKhatri097@gmail.com>
2025-02-07 11:36:45 -0500

    15 commit

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+// Adding an event listener to make the arrow clickable
+document.querySelector('.arrow').addEventListener('click', function() {
+    // You can add an animation or scrolling effect here. Example:
+    window.scrollBy({
+        top: window.innerHeight,  // scroll by the height of the window
+        behavior: 'smooth'
+    });
+});
